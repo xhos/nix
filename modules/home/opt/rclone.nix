b94b8106c70f338575fc1bd248d5e10ce930db91
xhos <60789741+xhos@users.noreply.github.com>
2026-01-08 15:56:35 -0500

    rclone tweaks

--- modules/home/opt/rclone.nix
+++ modules/home/opt/rclone.nix
@@ -5,31 +5,34 @@
   ...
 }:
 let
-  # theese need to be created manually
   onedriveDir = "/home/xhos/onedrive";
   username = "xhos";
 in {
-  options.rclone.enable = lib.mkEnableOption "rclone cloud storage mount (onedrive)";
-  config = lib.mkIf config.rclone.enable {
+  options.modules.rclone.enable = lib.mkEnableOption "rclone cloud storage mount (onedrive)";
+  
+  config = lib.mkIf config.modules.rclone.enable {
     persist.dirs = [ onedriveDir ];
     sops.secrets.rclone.path = "/home/${username}/.config/rclone/rclone.conf";
-    environment.systemPackages = with pkgs; [rclone];
-
-    systemd.services.rclone-onedrive-mount = {
-      wantedBy = ["default.target"];
-      after = ["network-online.target"];
-      requires = ["network-online.target"];
-
-      serviceConfig = {
+    home.packages = with pkgs; [ rclone ];
+    
+    systemd.user.services.rclone-onedrive-mount = {
+      Unit = {
+        Description = "rclone OneDrive mount";
+        After = [ "default.target" ];  # Just wait for user session to be ready
+      };
+      
+      Service = {
         Type = "simple";
         ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${onedriveDir}";
         ExecStart = "${pkgs.rclone}/bin/rclone mount --vfs-cache-mode full onedrive: ${onedriveDir}";
         ExecStop = "/run/current-system/sw/bin/fusermount -u ${onedriveDir}";
         Restart = "on-failure";
         RestartSec = "10s";
-        User = username;
-        Group = "users";
-        Environment = ["PATH=/run/wrappers/bin/:$PATH"];
+        Environment = "PATH=/run/wrappers/bin/:$PATH";
+      };
+      
+      Install = {
+        WantedBy = [ "default.target" ];
       };
     };
   };
