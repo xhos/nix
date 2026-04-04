dd6b2631d313a726c5dc338c892ea814e24ad933
xhos <60789741+xhos@users.noreply.github.com>
2026-04-03 23:33:04 -0400

    add headscale

--- systems/arashi/configuration.nix
+++ systems/arashi/configuration.nix
@@ -20,5 +20,32 @@
     memoryPercent = 25;
   };
 
+  services.headscale = {
+    enable = true;
+    address = "127.0.0.1";
+    port = 8080;
+    settings = {
+      server_url = "https://hs.xhos.dev";
+      ip_prefixes = ["100.64.0.0/10"];
+      dns = {
+        magic_dns = true;
+        base_domain = "lab.xhos.dev";
+        nameservers.global = ["1.1.1.1" "1.0.0.1"];
+      };
+    };
+  };
+
+  services.caddy = {
+    enable = true;
+    email = "lets-encrypt@xhos.dev";
+  };
+
+  services.caddy.virtualHosts."hs.xhos.dev".extraConfig = ''
+    reverse_proxy 127.0.0.1:8080
+  '';
+
+  networking.firewall.allowedTCPPorts = [80 443];
+  networking.firewall.allowedUDPPorts = [41641];
+
   system.stateVersion = "25.11";
 }
