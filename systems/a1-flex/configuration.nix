9963584d204271277148e71c0ba02d0d7cab80b6
xhos <60789741+xhos@users.noreply.github.com>
2026-04-03 20:48:18 -0400

    update a1-flex iso builder

--- systems/a1-flex/configuration.nix
+++ systems/a1-flex/configuration.nix
@@ -9,6 +9,7 @@
     "${inputs.nixpkgs}/nixos/modules/virtualisation/oci-image.nix"
   ];
 
+  # cptofs hardcodes mem=100M in C source, not enough for a full NixOS closure
   nixpkgs.overlays = [
     (final: prev: {
       lkl = prev.lkl.overrideAttrs (old: {
@@ -22,31 +23,40 @@
     })
   ];
 
+  # oci-image.nix never passes memSize to make-disk-image.nix (nixpkgs#479591)
+  # also strip kvm requirement as OCI ARM VMs don't expose /dev/kvm
   system.build.OCIImage = lib.mkForce (
-    import "${inputs.nixpkgs}/nixos/lib/make-disk-image.nix" {
+    (import "${inputs.nixpkgs}/nixos/lib/make-disk-image.nix" {
       inherit config lib pkgs;
       inherit (config.virtualisation) diskSize;
       name = "oci-image";
       baseName = config.image.baseName;
-      configFile = builtins.path {
-        name = "oci-config-user.nix";
-        path = "${inputs.nixpkgs}/nixos/modules/virtualisation/oci-config-user.nix";
-      };
       format = "qcow2";
       partitionTableType =
         if config.oci.efi
         then "efi"
         else "legacy";
       memSize = 8192;
-    }
+      configFile = builtins.path {
+        name = "oci-config-user.nix";
+        path = "${inputs.nixpkgs}/nixos/modules/virtualisation/oci-config-user.nix";
+      };
+    }).overrideAttrs (old: {
+      requiredSystemFeatures = lib.filter (f: f != "kvm") (old.requiredSystemFeatures or []);
+      nativeBuildInputs =
+        map
+        (p:
+          if (p.pname or "") == "qemu-kvm"
+          then pkgs.qemu
+          else p)
+        (old.nativeBuildInputs or []);
+    })
   );
 
   networking.hostName = "a1-flex";
   nixpkgs.hostPlatform = "aarch64-linux";
 
   virtualisation.diskSize = 8192;
-
-  # NO lib.mkForce override needed anymore
 
   users.users.root.openssh.authorizedKeys.keyFiles = [../arashi/arashi.pub];
   services.openssh.settings.PermitRootLogin = "yes";
