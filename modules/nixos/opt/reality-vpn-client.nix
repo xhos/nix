92a5d81b7da419c767b0f845f892e2e767977152
xhos <60789741+xhos@users.noreply.github.com>
2026-04-15 02:08:23 -0400

    vpn to debug lvl

--- modules/nixos/opt/reality-vpn-client.nix
+++ modules/nixos/opt/reality-vpn-client.nix
@@ -25,7 +25,7 @@ in {
     services.sing-box = {
       enable = true;
       settings = {
-        log.level = "info";
+        log.level = "debug";
         dns = {
           servers = [
             {
