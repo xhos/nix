1423a85193ec34218643b8a2dad81395a9fd8440
xhos <60789741+xhos@users.noreply.github.com>
2026-04-22 00:40:21 -0400

    fancy systemd boot

--- modules/nixos/opt/bootloaders/systemd-boot.nix
+++ modules/nixos/opt/bootloaders/systemd-boot.nix
@@ -1,17 +1,30 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options.bootloader.systemd-boot.enable = lib.mkEnableOption "enable systemd-boot";
 
   config.boot = lib.mkIf config.bootloader.systemd-boot.enable {
-    plymouth.enable = true;
+    kernelParams = ["quiet" "splash" "rd.udev.log_level=3" "udev.log_priority=3"];
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+
+    plymouth = {
+      enable = true;
+      theme = "hexagon";
+      themePackages = [
+        (pkgs.adi1090x-plymouth-themes.override {
+          selected_themes = ["hexagon"];
+        })
+      ];
+    };
     loader = {
+      timeout = 0; # skip the bootloader unless i hold down a key
       efi.canTouchEfiVariables = true;
       systemd-boot = {
         enable = true;
-        configurationLimit = 5;
         consoleMode = "auto";
         rebootForBitlocker = true;
       };
