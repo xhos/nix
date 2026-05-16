02abdc52a60b2be5bf00f8403007c8e2977a90e0
xhos <60789741+xhos@users.noreply.github.com>
2026-05-16 14:05:57 +0800

    fix mime

--- modules/home/opt/mime.nix
+++ modules/home/opt/mime.nix
@@ -16,6 +16,78 @@
   videoPlayer = "io.github.celluloid_player.Celluloid.desktop";
   archiveManager = "org.gnome.FileRoller.desktop";
 
+  imageTypes = [
+    "image/jpeg"
+    "image/png"
+    "image/gif"
+    "image/webp"
+    "image/tiff"
+    "image/bmp"
+    "image/svg+xml"
+    "image/svg+xml-compressed"
+    "image/avif"
+    "image/heic"
+    "image/jxl"
+    "image/x-tga"
+    "image/x-dds"
+    "image/vnd-ms.dds"
+    "image/vnd.microsoft.icon"
+    "image/vnd.radiance"
+    "image/x-exr"
+    "image/x-portable-bitmap"
+    "image/x-portable-graymap"
+    "image/x-portable-pixmap"
+    "image/x-portable-anymap"
+    "image/qoi"
+    "image/x-qoi"
+  ];
+  audioTypes = [
+    "audio/mpeg"
+    "audio/mp4"
+    "audio/aac"
+    "audio/flac"
+    "audio/ogg"
+    "audio/opus"
+    "audio/vorbis"
+    "audio/webm"
+    "audio/wav"
+    "audio/x-wav"
+    "audio/x-matroska"
+  ];
+  videoTypes = [
+    "video/mp4"
+    "video/mpeg"
+    "video/x-matroska"
+    "video/webm"
+    "video/quicktime"
+    "video/x-msvideo"
+    "video/x-flv"
+    "video/x-ms-wmv"
+    "video/3gpp"
+    "video/ogg"
+  ];
+  textTypes = [
+    "text/plain"
+    "text/markdown"
+    "text/x-markdown"
+    "application/json"
+    "application/toml"
+    "application/x-yaml"
+    "text/x-python"
+    "text/x-shellscript"
+    "text/x-c"
+    "text/x-c++"
+    "text/x-go"
+    "text/x-rust"
+    "text/x-nix"
+    "text/css"
+    "application/javascript"
+    "application/xml"
+    "text/xml"
+  ];
+
+  assoc = types: app: lib.listToAttrs (map (t: lib.nameValuePair t app) types);
+
   # calibre, please, don't open .go files in a fucking ebook reader
   makeCalibreDesktop = name: exec: icon: ''
     [Desktop Entry]
@@ -32,26 +104,27 @@ in {
     mime.enable = true;
     mimeApps = {
       enable = true;
-      defaultApplications = {
-        "inode/directory" = fileManager;
-        "text/*" = editor;
-        "image/*" = imageViewer;
-        "audio/*" = videoPlayer;
-        "video/*" = videoPlayer;
-        "text/html" = browser;
-        "application/pdf" = pdfViewer;
-        "x-scheme-handler/http" = browser;
-        "x-scheme-handler/https" = browser;
-        "x-scheme-handler/about" = browser;
-        "x-scheme-handler/unknown" = browser;
-        "x-scheme-handler/terminal" = terminal;
-        "application/zip" = archiveManager;
-        "application/x-7z-compressed" = archiveManager;
-        "application/x-tar" = archiveManager;
-        "application/gzip" = archiveManager;
-        "application/x-rar" = archiveManager;
-        "application/x-xz" = archiveManager;
-      };
+      defaultApplications =
+        assoc imageTypes imageViewer
+        // assoc audioTypes videoPlayer
+        // assoc videoTypes videoPlayer
+        // assoc textTypes editor
+        // {
+          "inode/directory" = fileManager;
+          "text/html" = browser;
+          "application/pdf" = pdfViewer;
+          "x-scheme-handler/http" = browser;
+          "x-scheme-handler/https" = browser;
+          "x-scheme-handler/about" = browser;
+          "x-scheme-handler/unknown" = browser;
+          "x-scheme-handler/terminal" = terminal;
+          "application/zip" = archiveManager;
+          "application/x-7z-compressed" = archiveManager;
+          "application/x-tar" = archiveManager;
+          "application/gzip" = archiveManager;
+          "application/x-rar" = archiveManager;
+          "application/x-xz" = archiveManager;
+        };
 
       associations.removed = let
         calibreApps = [
