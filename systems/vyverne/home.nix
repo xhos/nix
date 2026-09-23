68bf7ec3a4e06fe0032f7f954b031dd5466f1b77
xhos <60789741+xhos@users.noreply.github.com>
2026-09-23 12:22:53 -0400

    change vyverne wp

--- systems/vyverne/home.nix
+++ systems/vyverne/home.nix
@@ -5,8 +5,8 @@
   # };
   #
   stylix.image = pkgs.fetchurl {
-    url = "https://w.wallhaven.cc/full/5g/wallhaven-5g1ep5.jpg";
-    sha256 = "sha256-0r+WD32FdoXAwqg+5t4F9JLmIAtA0DO9t3hnafjRnaI=";
+    url = "https://w.wallhaven.cc/full/og/wallhaven-ogj1gl.jpg";
+    sha256 = "sha256-FXNqSqynxQPF6zNjGXymzLy+a7iaDZdR7M95wmWrisY=";
   };
   # stylix.base16Scheme = ./min-dark.yaml;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/material-darker.yaml";
